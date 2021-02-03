c6963b612fcfa3ced2ea0e4182305c4e15f3b3e1
Kbdman <lihai1234@live.cn>
2021-02-03 22:57:03 +0800

    finish QuickSort

--- cpp/codes/QuickSort.cpp
+++ cpp/codes/QuickSort.cpp
@@ -1,9 +1,10 @@
+#include <iostream>
 void QuickSort(int* array,int size)
 {
-    if(size==1)
+    if(size<=1)
         return;
     int tmp=array[0];
-    int* back=&(array[size-i])
+    int* back=&(array[size-1]);
     int* front=array;
     while(front!=back)
     {
@@ -15,13 +16,13 @@ void QuickSort(int* array,int size)
         *back=*front;
     }
     *front=tmp;
-    int idx=front-array
+    int idx=front-array;
     QuickSort(array,idx);
-    QuickSort(array+idx+1,size-i-1);
+    QuickSort(array+idx+1,size-idx-1);
 }
 int main()
 {
-    int ar[]={1,43,6,213,654,32,645,4321};
+    int ar[]={1,43,336,213,654,32,645,4321};
     QuickSort(ar,8);
     for(int i=0;i<8;i++)
     {
